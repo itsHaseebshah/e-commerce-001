0a0b03688f5073335bf045719b7d6ab656b3cd2f
itsHaseebshah <121252149+itsHaseebshah@users.noreply.github.com>
2024-02-24 23:56:52 +0000

    eslintrc.js has been changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,22 @@
 module.exports = {
-  root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
-  plugins: 'prettier/prettier':['error', {endOfLine: 'auto'}],
-  'no-console':'off', 
-}
+    root: true,
+    extends: ['plugin:@next/next/recommended', '@payloadcms'],
+    ignorePatterns: ['**/payload-types.ts'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'no-console': 'off',
+    },
+  }
+
+
+
+
+// module.exports = {
+//     root: true,
+//     extends: ['plugin:@next/next/recommended', '@payloadcms'],
+//     ignorePatterns: ['**/payload-types.ts'],
+//     plugins: 'prettier/prettier':['error', {endOfLine: 'auto'}],
+//     'no-console':'off', 
+//   }
+  
